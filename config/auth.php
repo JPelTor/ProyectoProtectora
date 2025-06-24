b4599b8cbd0bafd6e8f70470fedfa0a52b115a9b
JPelTor <jorpeltor@alu.edu.gva.es>
2025-06-25 01:39:36 +0200

    commit

--- config/auth.php
+++ config/auth.php
@@ -64,7 +64,7 @@ return [
     */
 
     'providers' => [
-        'usuarios' => [
+        'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\Usuario::class,
         ],
